4912225396d147ca4d7d676f4da06ba16cf93138
RubenCantos <57601197+RubenCantos@users.noreply.github.com>
2021-09-04 12:02:02 +0200

    Update calculadora.js
    changed variable types

--- calculadora.js
+++ calculadora.js
@@ -1,28 +1,28 @@
 const crearTabla = () => {
   tabla = document.getElementById("tabla");
   if (tabla) {
-    padre = tabla.parentNode;
+    let padre = tabla.parentNode;
     padre.removeChild(tabla);
   }
 
   const aleloInput1 = document.getElementById("alelo1");
   const aleloInput2 = document.getElementById("alelo2");
-  alelo1 = aleloInput1.value;
-  alelo2 = aleloInput2.value;
+  const alelo1 = aleloInput1.value;
+  const alelo2 = aleloInput2.value;
 
   const combinaciones = (alelo1 + alelo2).length;
 
-  var crear = document.getElementById("crear");
-  var tabla = document.createElement("table");
-  var tblBody = document.createElement("tbody");
+  const crear = document.getElementById("crear");
+  const tabla = document.createElement("table");
+  const tblBody = document.createElement("tbody");
 
   tabla.setAttribute("id", "tabla");
 
   for (var i = 0; i < alelo1.length; i++) {
     var hilera = document.createElement("tr");
     for (var j = 0; j < alelo2.length; j++) {
-      var celda = document.createElement("td");
-      var textoCelda = document.createTextNode(alelo1[i] + alelo2[j]);
+      let celda = document.createElement("td");
+      let textoCelda = document.createTextNode(alelo1[i] + alelo2[j]);
       celda.appendChild(textoCelda);
       hilera.appendChild(celda);
     }
